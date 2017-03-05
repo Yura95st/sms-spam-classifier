3aca5b4d30f6306e042cf51b489102fa4cdbf4ed
yura95st <yura95st@gmail.com>
2017-03-05 18:44:57 +0200

    Implement reading data feature.

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-def main():
-    pass
+from message_type import MessageType
+import string
+
+
+def read_data(file_path):
+    translator = str.maketrans({p: ' ' for p in string.punctuation})
+
+    with open(file_path, 'r') as data_file:
+        for line in data_file:
+            line = line.translate(translator).split()
+
+            msg_type = MessageType.ham if line[
+                0] == 'ham' else MessageType.spam
+            msg_text = line[1:]
+
+            yield msg_type, msg_text
+
+
+def main(file_path='data/SMSSpamCollection'):
+    messages = []
+
+    for msg in read_data(file_path):
+        messages.append(msg)
 
 
 if __name__ == '__main__':
---
+++ message_type.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class MessageType(Enum):
+    ham = 0
+    spam = 1
